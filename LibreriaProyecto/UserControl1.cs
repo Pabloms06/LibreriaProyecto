4ee2e84f662647aec1132f7ad86e8fad44b3aa1a
pablo <pablomarcossanchez06.pm@gmail.com>
2021-02-11 14:07:31 +0100

    Agregar archivos de proyecto.

---
+++ LibreriaProyecto/UserControl1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LibreriaProyecto
+{
+
+
+
+
+    public partial class UserControl1 : UserControl
+
+    {
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> colores = new List<string>();
+
+            colores.Add("red");
+            colores.Add("yellow");
+            colores.Add("green");
+            colores.Add("blue");
+
+            if (textBox1.Text.Equals(colores[0]))
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+
+            if (textBox1.Text.Equals(colores[1]))
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+
+            if (textBox1.Text.Equals(colores[2]))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+
+            if (textBox1.Text.Equals(colores[3]))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+
+
